ba1a5a5fe538104855b5bae82a5cd279eb416765
OliverAgB <oliveragervig@gmail.com>
2025-05-22 12:02:29 +0200

    d

--- Models/Users.cs
+++ Models/Users.cs
@@ -2,5 +2,11 @@
 {
     public class Users
     {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public int phone_code { get; set; }
+        public string phone_number { get; set; }
+
     }
 }
