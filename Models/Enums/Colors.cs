6784f6d322dfefd466916c118a4ce614adf75841
mikk922r <mikk922r@edu.mercantec.dk>
2025-05-26 11:33:47 +0200

    filter binding values

---
+++ Models/Enums/Colors.cs
@@ -0,0 +1,17 @@
+﻿namespace Projekt.Models
+{
+    public enum Colors
+    {
+        Black = 0,
+        White = 1,
+        Gray = 2,
+        Red = 3,
+        Blue = 4,
+        Green = 5,
+        Yellow = 6,
+        Purple = 7,
+        Pink = 8,
+        Orange = 9,
+        Brown = 10,
+    }
+}
