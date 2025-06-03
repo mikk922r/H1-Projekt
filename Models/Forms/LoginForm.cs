703415549d1da172ce02af554376435c58baa19c
mikk922r <mikk922r@edu.mercantec.dk>
2025-06-03 16:30:40 +0200

    Password fejlbesked på login formen

--- Models/Forms/LoginForm.cs
+++ Models/Forms/LoginForm.cs
@@ -10,7 +10,7 @@ namespace Projekt.Models.Forms
 
         [Required(ErrorMessage = "Adgangskode er påkrævet")]
         [DataType(DataType.Password)]
-        [MinLength(8)]
+        [MinLength(8, ErrorMessage = "Adgangskode skal være mindst 8 tegn langt")]
         public string Password { get; set; } = string.Empty;
     }
 }
